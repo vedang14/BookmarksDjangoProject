d9bb0fe8c4506614830a6c8ba95c06649f683511
vedang bhardwaj <vedang.bhardwaj14@gmail.com>
2020-07-17 22:34:18 +0530

    secondCommits

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -4,10 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import logout
 from django.template import Context
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from bookmarks.forms import *
 def main_page(request):
     # output = '''
     #     <html>
@@ -21,14 +25,19 @@ def main_page(request):
     #     'Welcome to Django Bookmarks',
     #     'Where you can store and share bookmarks!'
     # )
-    template = get_template('main_page.html')
-    Context = {
-        'head_title' : 'Django Bookmarks',
-        'page_title':  'Welcome to Django Bookmarks',
-        'page_body' : 'Where you can store and share bookmarks!'
-    }
-    output = template.render(Context)
-    return HttpResponse(output)
+    # template = get_template('main_page.html')
+    # Context = {
+    #     # 'head_title' : 'Django Bookmarks',
+    #     # 'page_title':  'Welcome to Django Bookmarks',
+    #     # 'page_body' : 'Where you can store and share bookmarks!'
+    #     'user' : request.user
+    # }
+    # output = template.render(Context)
+    # return HttpResponse(output)
+
+    return render(
+        request,'main_page.html'
+    )
 
 def user_page(request,username):
     try:
@@ -36,10 +45,30 @@ def user_page(request,username):
     except:
         raise Http404('Requested user not found')
     bookmarks = user.bookmark_set.all()
-    template = get_template('user_page.html')
-    Context = {
+    # template = get_template('user_page.html')
+    variable = RequestContext(request,{
         'username' : username,
         'bookmarks': bookmarks
-    }
-    output = template.render(Context)
-    return HttpResponse(output)
+    })
+    # output = template.render(RequestContext)
+    # return HttpResponse(output)
+    return render(request,'user_page.html',variable)
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid() :
+            user = User.objects.create_user(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+                email = form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = { 'form' : form}
+    return render(request,'registration/register.html',variables)     
--- django_bookmarks/django_bookmarks/urls.py
+++ django_bookmarks/django_bookmarks/urls.py
@@ -13,11 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import os.path 
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login
 from bookmarks.views import *
+from bookmarks.views import *
+from django.views.static import serve
+from django.views.generic import TemplateView
+
+site_media = os.path.join(os.path.dirname(__file__),'site_media')
+variable = {
+    'document_root' : site_media
+}
+var2 = {'template': 'registration/register_success.html'}
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$',main_page),
     url(r'^user/(\w+)/$', user_page),
+    url(r'^login/$',login,name='login'),
+    url(r'^logout/$',logout_page),
+    url(r'^site_media/(?P<path>.*)$',serve,variable),
+    url(r'^register/$',register_page),
+    url(r'^register/success/$', TemplateView.as_view(template_name ='registration/register_success.html'),var2),
 ]
